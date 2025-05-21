7665a1d99e7482857d717753b1fa0b79ef8af56f
Gooijer <niek.de.gooijer@centric.eu>
2025-05-21 11:27:55 +0200

    feat: pr comments resolved

--- example-webapp/Models/User.cs
+++ example-webapp/Models/User.cs
@@ -1,9 +1,9 @@
 using Russkyc.MinimalApi.Framework.Core;
-namespace example_webapp.Shared.Models;
+namespace example_webapp.Models;
 
 public class User : DbEntity<int>
 {
-    public int Id { get; set; } // Unique identifier for the user
+    // public int Id { get; set; } // Unique identifier for the user
     public string FirstName { get; set; } // User's first name
     public string LastName { get; set; } // User's last name
     public string Email { get; set; } // User's email address

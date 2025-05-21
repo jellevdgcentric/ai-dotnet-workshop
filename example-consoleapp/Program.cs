8600f0df43879001e3842f50170187b08a748303
Jelle van der Gulik <jelle.van.der.gulik@centric.eu>
2025-05-21 13:33:55 +0200

    Moved console solution file into example consoleapp

---
+++ example-consoleapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+const string endpoint = "https://azureaiservicesworkshop.openai.azure.com/";
+const string apiKey = "4TkMyWgSeqKZkSgONCZAxLhndWkSqKxXkubZkcf8BRUbT0vq41aoJQQJ99BAACfhMk5XJ3w3AAAAACOGyExw";
+Uri endpointUri = new Uri(endpoint);
+
+AzureOpenAIClient client = new AzureOpenAIClient(endpointUri, new Azure.AzureKeyCredential(apiKey), new AzureOpenAIClientOptions());
+ChatClient chatClient = client.GetChatClient("gpt-4.1");
+
+ChatCompletion response = await chatClient.CompleteChatAsync(new UserChatMessage("Hello, how are you?"));
+string[] responseTextArray = response.Content.Select(m => m.Text).ToArray();
+string responseText = string.Join(", ", responseTextArray);
+Console.WriteLine(responseText);
+
+Console.ReadLine();

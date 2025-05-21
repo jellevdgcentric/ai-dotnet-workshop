be234d1322236f13b1a63503354cd0c06de731d9
Jelle van der Gulik <jelle.van.der.gulik@centric.eu>
2025-05-21 19:19:09 +0200

    Workshop file

--- example-consoleapp/Program.cs
+++ example-consoleapp/Program.cs
@@ -29,3 +29,40 @@ Console.WriteLine($"Search Endpoint: {searchEndpoint}");
 Console.WriteLine($"Search API Key: {searchApiKey[..6]}...");
 Console.WriteLine($"iRacing Vector Name: {iRacingVectorName}");
 Console.WriteLine($"Blades in the Dark Vector Name: {bladesVectorName}");
+
+AzureKeyCredential azureKeyCredential = new AzureKeyCredential(apiKey);
+AzureOpenAIClient azureOpenAIClient = new AzureOpenAIClient(new Uri(azureEndpoint), azureKeyCredential);
+
+ChatClient chatClient = azureOpenAIClient.GetChatClient(model);
+
+string input = Console.ReadLine() ?? "";
+
+
+// Semantic search
+AzureKeyCredential semanticKeyCredential = new AzureKeyCredential(searchApiKey);
+Uri searchUriEndpoint = new Uri(searchEndpoint);
+SearchClient searchClient = new SearchClient(searchUriEndpoint, bladesVectorName, semanticKeyCredential);
+SearchResults<SearchDocument> results = await searchClient.SearchAsync<SearchDocument>(input, new SearchOptions()
+{
+    Size = 10
+});
+
+var resultTextList = results.GetResults().Select(result => result.Document["content"]).ToList();
+string resultText = string.Join("\n\n", resultTextList);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Search Results:" + resultText);
+
+// Response
+string formattedInput = "### User Query: " + input + "\n\n### Search Results: " + resultText;
+
+SystemChatMessage systemPrompt = new SystemChatMessage("You are a helpful Blades in the Dark assistant, " +
+    "you are only allowed to respond with information that is provided in the Search Results.");
+UserChatMessage userPrompt = new UserChatMessage(formattedInput);
+
+ChatMessage[] chatMessages = new ChatMessage[] { systemPrompt, userPrompt };
+ChatCompletion chatCompletion = await chatClient.CompleteChatAsync(chatMessages);
+string[] responseArray = chatCompletion.Content.Select(completion => completion.Text).ToArray();
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine($"Response: {string.Join(", ", responseArray)}");
